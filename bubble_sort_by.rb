375f87c15cebb87267f62d75ee2b200ada497704
Mauricio Santos <maosan132@gmail.com>
2020-04-08 17:42:33 -0500

    bubble sort using yield and sort_by

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,21 +1,19 @@
-def
-  swap=true
-    while swap do
-    swap = false
-    yield
-    (arr.length-1).times do |i|
-      if arr[i] > arr[i+1]
-        arr[i], arr[i+1] = arr[i+1], arr[i]
-        swap=true 
+def bubble_sort_by(arr)
+  i = 0
+  arr.length.times do
+    (arr.length - 1).times do
+      block_vlaue = yield(arr[i], arr [i + 1])
+      if block_vlaue.positive?
+        temp = arr[i + 1]
+        arr[i + 1] = arr[i]
+        arr[i] = temp
       end
+      i += 1
     end
-   arr
+    i = 0
   end
+  arr
 end
-  
-  
-bubble_sort([2,3,4,8,1,2,9])
-bubble_sort do |i|
-  i
-end
-puts arr
+
+p bubble_sort_by(%w[whats_up hello hi ]) { |left, right| left.length - right.length }
+
