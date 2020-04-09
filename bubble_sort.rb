448291eda756815fefa8cc8a21e655f4757e2b6c
Hemant soni <sonihemant4741@gmail.com>
2020-04-09 22:33:44 +0530

    stikler issue resolved

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,16 +1,15 @@
 def bubble_sort(arr)
-     swap=true
-    while swap
-      swap = false
-      (arr.length-1).times do |i|
-        if arr[i] > arr[i+1]
-          arr[i], arr[i+1] = arr[i+1], arr[i]
-          swap=true
-        end
-      end
-    end
-    arr
+ swap = true
+   while swap
+    swap = false
+     (arr.length - 1).times do |i|
+       if arr[i] > arr[i + 1]
+         arr[i], arr[i + 1] =  arr[i + 1], arr[i]
+         swap = true
+       end
+     end
+  end
+  arr
 end
-
 
 bubble_sort([2, 3, 4, 8, 1, 2, 9])
